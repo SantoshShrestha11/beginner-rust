a244f6f1d0819a99aea1b247920ca846c3c6b753
Santosh <santoshsantoshsthastha@gmail.com>
2025-01-02 20:49:37 +0545

    learned about struct

--- Struct/src/main.rs
+++ Struct/src/main.rs
@@ -1,3 +1,19 @@
+struct User{
+    username : String,
+    email: String,
+    sign_in_count: u64,
+    active: bool
+}
 fn main() {
-    println!("Hello, world!");
+    let mut user1=User{
+        email: String::from("santosh@gmail.com"),
+        username: String::from("Santosh Shrestha"),
+        sign_in_count: 1,
+        active: true
+    };
+    let name = user1.username;
+    println!("The name of the user is {name}.");
+    user1.username = String::from("Sanish Shrestha");
+    println!("The name of user after changing is {0}.",user1.username);
+    println!("The name of the uer after changing too but the refrence is different {0}.",name);
 }
