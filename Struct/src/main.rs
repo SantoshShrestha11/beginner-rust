bdb5147710f606de28522f1d31567b1c6394eead
Santosh <santoshsantoshsthastha@gmail.com>
2025-01-02 21:52:47 +0545

    learned about struct and tuple structs

--- Struct/src/main.rs
+++ Struct/src/main.rs
@@ -13,7 +13,24 @@ fn main() {
     };
     let name = user1.username;
     println!("The name of the user is {name}.");
-    user1.username = String::from("Sanish Shrestha");
-    println!("The name of user after changing is {0}.",user1.username);
-    println!("The name of the uer after changing too but the refrence is different {0}.",name);
+
+    let user2: User = build_user(String::from("sanish@gmail.com"), String::from("sanish"));
+    let user3 = User{
+        email: String::from("anish@gmail.com"),
+        username: String::from("ansih"),
+        ..user2
+    };
+}
+
+fn build_user(email: String, username: String)->User{
+    User{
+        username,
+        email,
+        sign_in_count: 1,
+        active: true
+    }
+}
+fn tuple_structs() {
+    struct Color(i32,i32,i32);
+    struct Point(i32,i32,i32);
 }
