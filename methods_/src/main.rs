d0ea7b25285043b2189b62e6ea25029c42ab309a
Santosh <santoshsantoshsthastha@gmail.com>
2025-01-03 21:55:18 +0545

    learned about the getters in the methods
    exit

--- methods_/src/main.rs
+++ methods_/src/main.rs
@@ -8,10 +8,21 @@ impl Rectangle {
         self.width * self.height
     }
 }
+impl Rectangle {// we can give the name of the method same as one of the struct's fields.
+    // we can only return the vals in the fields and do nothing else .
+    // methods like this are called getteres
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+}
 fn main() {
     let rect1= Rectangle {
         width: 30,
         height: 20,
+
     };
+    if rect1.width () {
+        println!("the rectangle has a non zero width; it is {}", rect1.width);
+    }
     println!("The area of the rectangle is {}", rect1.area());
 }
