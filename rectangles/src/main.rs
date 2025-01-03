bbdc15643ddc1db1af6399f924dbc6d2b04cab47
Santosh <santoshsantoshsthastha@gmail.com>
2025-01-03 21:16:26 +0545

    learned about dbg! trait that is very essential to debug

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -18,23 +18,37 @@
 // fn area(dimension:(u32,u32)) -> u32 {
 //     dimension.0*dimension.1
 // }
+// #[derive(Debug)]
+// struct Rect{
+//     length: u32,
+//     width:u32
+// }
+// fn main() {
+//     let rect1 = Rect {
+//         length: 20,
+//         width: 30
+//     };
+// //The {:?} with the # prefix in println! is a valuable tool for inspecting and presenting your data in a clear and readable format,
+//     //aiding in both debugging and understanding your program's behavior.
+//     println!("The area is {}",area(&rect1));
+//     println!("rectangle {:#?}",rect1);
+//
+// }
+//
+// fn area(rectangle:&Rect) -> u32 {
+//     rectangle.length * rectangle.width
+// }
 #[derive(Debug)]
-struct Rect{
+struct Rectangle{
     length: u32,
-    width:u32
+    breadth: u32
 }
 fn main() {
-    let rect1 = Rect {
-        length: 20,
-        width: 30
+    let scale= 2;
+   let rectangle = Rectangle {
+         length: dbg!(20 * scale),
+        breadth: 30
     };
-//The {:?} with the # prefix in println! is a valuable tool for inspecting and presenting your data in a clear and readable format,
-    //aiding in both debugging and understanding your program's behavior.
-    println!("The area is {}",area(&rect1));
-    println!("rectangle {:#?}",rect1);
-
-}
-
-fn area(rectangle:&Rect) -> u32 {
-    rectangle.length * rectangle.width
+    dbg!(&rectangle);
+    
 }
