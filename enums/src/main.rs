9e1d90d3056fdcbd1e7b5cb2e9085ce5dc362077
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-01-07 13:20:02 +0545

    learned about the use of the struct to handel data of the types that is created using the enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,8 +1,34 @@
-enum IpAddrKind{
-    V4,
-    V6,
-}
+// enum IpAddrKind{
+//     V4,
+//     V6,
+// }
+// fn main() {
+//     let four = IpAddrKind::V4;
+//     let six = IpAddrKind::V6;
+//     
+//     route(IpAdrrKind::v4);
+//     route(IpAdrrKind::v6);
+//
+// }
+// fn route(ip_kind: IpAddrKind) {}
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+    enum IpAddrKind{
+        V4,
+        V6,
+    }
+    //creating a struct to handle the data type of IpAddrKind.
+    //the type ip_kind was created using the enume.
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
+
+    let home = IpAddr {
+        kind:IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
 }
