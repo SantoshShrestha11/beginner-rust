3afe37399a56209ba30f731a0acf22b438e9b0d3
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-01-13 22:16:39 +0545

    fixed some bugs and the spe

--- new_restaurant/src/lib.rs
+++ new_restaurant/src/lib.rs
@@ -64,7 +64,7 @@ mod back_of_house {
 }
 
 // A public function using all components
-pub fn eat_at_restruant() {
+pub fn eat_at_restaurant() {
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
 
--- new_restaurant/src/main.rs
+++ new_restaurant/src/main.rs
@@ -1,8 +1,8 @@
-use new_resturant::eat_at_restruant;
+use new_restaurant::eat_at_restaurant;
 fn main() {
     println!("Welcome to the resturant");
 
 
     //call the public funciton to demonstrate functionality
-    eat_at_restruant();
+    eat_at_restaurant();
 }
