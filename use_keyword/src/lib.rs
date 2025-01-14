135bf26f3fa8e0e30d1ec38bb88f4a5d29cdc178
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-01-14 15:19:02 +0545

    used the glob operator

--- use_keyword/src/lib.rs
+++ use_keyword/src/lib.rs
@@ -6,9 +6,14 @@ mod fornt_of_house {
 }
 
 #[allow(dead_code)]
-mod customer {
+pub mod customer {
 use crate::fornt_of_house::hosting;
+    //we can use this method and this method is called the idomatic method use paths 
     pub fn eat_at_restaurant() {
         hosting::add_to_waitlist();
+        //super::fornt_of_house::hosting::add_to_waitlist();
+        //this is the antoher method out there 
+        //here using super hosing to refrence parent module
+        //the second one is not good to use
     }
 }
--- use_keyword/src/main.rs
+++ use_keyword/src/main.rs
@@ -1,4 +1,12 @@
-use use_keyword::customer::{self, eat_at_restaurant};
+// use std::io;
+// use std::io::Write;
+//we can use the nested path like this way 
+//use std::io{self, wirte};
+/* use use_keyword::customer::{self, eat_at_restaurant}; */
+// use use_keyword::*;
+// we can use this * symbol to bring every pub this insider the use_keyword mod here
+// use use_keyword::customer;
+use use_keyword::customer::eat_at_restaurant;
 fn main() {
     eat_at_restaurant();
 }
