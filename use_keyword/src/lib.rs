2ce7b49dc8a2dfd555c7d49cec2de4076eebd068
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-01-14 10:18:22 +0545

    the use of the use keyword to bring the path in scope

---
+++ use_keyword/src/lib.rs
@@ -0,0 +1,14 @@
+#[allow(dead_code)]
+mod fornt_of_house {
+    pub mod hosting{
+        pub fn add_to_waitlist() {}
+    }
+}
+
+#[allow(dead_code)]
+pub mod customer {
+use crate::fornt_of_house::hosting;
+    pub fn eat_at_restaurant() {
+        hosting::add_to_waitlist();
+    }
+}
---
+++ use_keyword/src/main.rs
@@ -0,0 +1,4 @@
+use use_keyword::customer::{self, eat_at_restaurant};
+fn main() {
+    eat_at_restaurant();
+}
