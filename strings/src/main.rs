4994285224855c7e283553a0a78219907c6157b8
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-01-15 14:23:26 +0545

    done with the strings too by fixing the errors in the code

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,15 +1,15 @@
 fn main() {
     // creating a new , empty string
-    let mut s = String::new();
+    let s = String::new();
     println!("Empty string: {:?}", s);
 
     //creating a string form a string literal using to_string and 
     //using 'to_string' and String:form
     let data = "initial contents";
-    let s1 = data.to_sting();
+    let s1 = data.to_string();
     let s2 = String::from("Initial contents");
     println!("String created with to_string: {}", s1);
-    println!("String created with String::from; {}", s2);
+    println!("String created with String::from: {}", s2);
 
     // Storing greetings in different language
     let hello = String::from("こんにちは"); //hello in japanese
@@ -31,7 +31,7 @@ fn main() {
 
     //using 'format!' macro for complex concatenations
     let s7 = String::from("tic");
-    let s8 = String::form("tac");
+    let s8 = String::from("tac");
     let s9 = String::from("toe");
     let result = format!("{s7}-{s8}-{s9}");
     println!("Using format! macro: {}", result);
@@ -39,7 +39,7 @@ fn main() {
     //UTF-8 internal representation and lsicing 
     let hindi = "नमस्ते";
     println!("UTF-8 bytes: {:?}", hindi.as_bytes());
-    println!("unixode scalar values: {:?}", hindi.chars().collect::<vec<_>>());
+    println!("unixode scalar values: {:?}", hindi.chars().collect::<Vec<_>>());
 
     //slicing a valid porting of a UTF-8 string
     let slice = &hindi[0..6];//"नम"
@@ -50,6 +50,12 @@ fn main() {
     //let invalid_slice = &hindi[0..1];
 
     if let Some(first_char) = hindi.chars().nth(0) {
+        //here the hindi.char() modes creates an iterator ove the unicode 
+        //sclar values of the string hindi
+        //and the .nth() will return the elements at the specified zero-based index.
+        // incase of the .nth(0) this will retrives the first character of the string,
+        // if the string is empty then,
+        //it will return the None 
         println!("First character: {}", first_char);
     } else {
         println!("No first character found.");
