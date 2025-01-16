6c5747a4022d0c6314b12433fcaa71373218a526
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-01-16 17:36:08 +0545

    some mistakes are there gonna fix that later

--- converting_string_to_pig_latin/src/main.rs
+++ converting_string_to_pig_latin/src/main.rs
@@ -1,13 +1,24 @@
-fn main() {
-    let sentence = "Hello there, I am Santosh Shrestha .";
-    let vowels = ['a', 'e', 'i', 'o', 'u'];
+use unicode_segmentation::UnicodeSegmentation;
 
-    for word in sentence.split_whitespace() {
-        if let some(first_char) = word.chars().next() {
-            if vowels.contains(&first_char.to_ascii_lowercase()) {
+fn to_pig_latin(input: &str) -> String{
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    let mut result = String::new();
 
-            }
+    for word in input.split_whitespace() {
+        let first_char = word.chars().next().unwrap();
+        if vowels.contains(&first_char) {
+            result.push_str(&format!("{word}-hay"));
+        } else {
+            let mut chars = word.graphemes(true);
+            let first_cluster = chars.next().unwrap();
+            let rest: String = chars.collect();
+            result.push_str(&format!("{rest}-{first_cluster}ay "));
         }
     }
-
+    result.trim_end().to_string()
+}
+fn main() {
+    let sentence = "Hello there , My name is Santosh Shrestha.";
+    let pig_latin_sentence = to_pig_latin(sentence);
+    println!("Pig latin: {}", pig_latin_sentence);
 }
--- mode_and_median_usinghasmap/src/main.rs
+++ mode_and_median_usinghasmap/src/main.rs
@@ -1,39 +1,40 @@
 use std::collections::HashMap;
-fn find_median_and_mode(mut integers: Vec<u32>) -> (f64, u32) {
-    //sorting the vector
+
+fn find_median_and_mode(mut integers: Vec<i32>) -> (f64, i32) {
+    // Sorting the vector
     integers.sort();
 
-    //calculating the median here
+    // Calculating the median
     let median = match integers.len() {
         0 => 0.0,
         even if even % 2 == 0 => {
-            let mid1 = integers[even / 2 -1];
+            let mid1 = integers[even / 2 - 1];
             let mid2 = integers[even / 2];
-        (mid1 as f64 + mid2 as f64)/ 2.0
-         },
-        odd if odd % 2 != 0 => integers[integers.len() / 2] as f64,
+            (mid1 as f64 + mid2 as f64) / 2.0
+        },
+        odd => integers[integers.len() / 2] as f64, 
     };
-    //calculating hte mode using a HashMap
+
+    // Calculating the mode using a HashMap
     let mut frequencies = HashMap::new();
     for &integer in &integers {
         *frequencies.entry(integer).or_insert(0) += 1;
     }
 
-    let mut mode = integers[0];
+    let mut mode: i32 = integers[0];
     let mut max_count = 0;
     for (&integer, &count) in &frequencies {
         if count > max_count {
-            mode = integer;
+            mode = *integer;
             max_count = count;
         }
     }
-    (median, mode)
-    
 
+    (median, mode)
 }
 
 fn main() {
-    let integers = vec![1,2,3,1,2,31,1,34,4,5];
+    let integers = vec![1, 2, 3, 1, 2, 31, 1, 34, 4, 5];
     let (median, mode) = find_median_and_mode(integers);
     println!("Median: {}", median);
     println!("Mode: {}", mode);
