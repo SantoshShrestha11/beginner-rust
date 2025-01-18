5ffe19eb5c2a652394cea67331fb228f35be6d5d
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-01-18 11:16:33 +0545

    learned about the way you can take the input form the user and comvert it into the vector and make use of it

--- input_of_the_vector/src/main.rs
+++ input_of_the_vector/src/main.rs
@@ -6,7 +6,10 @@ fn main() {
         .read_line(&mut something)
         .expect("failed to take the input form you ");
     println!("the thing that you have inputed is {}", something);
-    let some_vec: Vec<&str> = something.split_whitespace().collect();
+    // let some_vec: Vec<&str> = something.split_whitespace().s.parse<&i32>.collect();
+    let some_vec: Vec<&int> =something;
+        .something.split_whitespace()
+        .filter_map(|&s| s.parse::<i32>().ok());
     println!("the data inside the some-vec is {:?}",some_vec);
 
     for words in some_vec {
