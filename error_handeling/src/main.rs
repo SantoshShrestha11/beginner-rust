0b289f228c6274dc24c6bd45071c0ccfd032bfd2
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-01-18 17:22:08 +0545

    the match and map are kinda simmilar

--- error_handeling/src/main.rs
+++ error_handeling/src/main.rs
@@ -5,10 +5,13 @@ fn main() {
     io::stdin()
         .read_line(&mut string)
         .expect("you got some error while taking the string");
-    let nums:Result<i32, _> = string.split_whitespace().parse();
+    let nums: Result<Vec<i32>, _> = string
+        .split_whitespace()
+        .map(|s| s.parse::<i32>())
+        .collect();
     match nums{
-        Ok(number) => println!("the number taken as integer is {number}"),
-        Err(error) => println!("you got some error while parsing {error}"),
+        Ok(number) => println!("the number taken as integer is {:#?}",number),
+        Err(e) => println!("you got some error while parsing {e}"),
     }
 
 }
