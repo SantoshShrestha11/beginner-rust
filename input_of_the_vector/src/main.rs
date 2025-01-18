ed40c97b724a12610529df50bdb6bdec9603a0f2
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-01-18 12:15:25 +0545

    some changes and fixes

--- input_of_the_vector/src/main.rs
+++ input_of_the_vector/src/main.rs
@@ -6,14 +6,13 @@ fn main() {
         .read_line(&mut something)
         .expect("failed to take the input form you ");
     println!("the thing that you have inputed is {}", something);
-    let some_vec: Vec<int> = something
+    let some_vec: Vec<i32> = something
         .split_whitespace()
-        .filter_map(|s| s.parse::<i32>().ok())
+        .map(|s| s.parse::<i32>().expect("failed to parse integer"))
         .collect();
     println!("the data inside the some-vec is {:?}",some_vec);
 
     if some_vec.is_empty() {
         println!("no valid integers were provided here");
-        return ;
     }
 }
