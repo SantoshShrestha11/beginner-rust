11603fa6d0592197937998603537b7e36f3a3bfb
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-01-24 09:27:45 +0545

    the crate trim

---
+++ trim/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let s = String::from("santohs shrestha");
+    let a = String::from(" hello there ");
+    let b = String::from(" hello again there ");
+    println!("{}",s.trim());
+    println!("{}",a.trim());
+    println!("{}",b.trim());
+    //trim will remove the white space in the beginning and end of the string
+}
