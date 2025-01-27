7d88f78588aced3b168be76198e7abffe27bfa77
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-01-27 17:23:23 +0545

    learned about some of the type of the panics and way to use the cfg

--- error_handeling/src/main.rs
+++ error_handeling/src/main.rs
@@ -25,3 +25,49 @@
 //     drink("lemonade");
 // }
 
+// #[cfg(target_os = "windows")]
+// fn windows_only() {
+//     println!("This only runs on Windows");
+// }
+//
+// #[cfg(target_os = "linux")]
+// fn linux_only() {
+//     println!("This only runs on Linux");
+// }
+// fn main() {
+//     linux_only();
+//     windows_only();
+// }
+// fn main() {
+//     if cfg!(target_os = "windows") {
+//         println!("Running on Windows!");
+//     } else {
+//         println!("Not running on Windows!");
+//     }
+//     if cfg!(target_os = "linux") {
+//         println!("Running on linux!");
+//     } else {
+//         println!("Not running on linux!");
+//     }
+// }
+
+fn drink(beverage: &str) {
+    // You shouldn't drink too much sugary beverages.
+    if beverage == "lemonade" {
+        //here are two types of the panics one is abort and another is unwind
+        //    unwind (default): The stack is unwound, destructors (drop) are called, and resources are cleaned up before terminating the thread.
+        // abort: The program immediately terminates without cleaning up resources, producing a smaller binary.
+        if cfg!(panic = "abort") {
+            println!("This is not your party. Run!!!!");
+        } else {
+            println!("Spit it out!!!!");
+        }
+    } else {
+        println!("Some refreshing {} is all I need.", beverage);
+    }
+}
+
+fn main() {
+    drink("water");
+    drink("lemonade");
+}
