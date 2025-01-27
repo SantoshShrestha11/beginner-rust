0afe367302b2ed8336adf4e03cad0385bb8836ad
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-01-27 20:09:22 +0545

    learned some thing more about the error handeling

--- error_handeling/src/main.rs
+++ error_handeling/src/main.rs
@@ -77,6 +77,8 @@
 // }
 ////here the type of the panic that we get is of unwind so another block of 
 ////code in not unwind is not taken in account
+////also the panic strategy can be set form the command line using abort or unwind 
+//// like this rustc projectname.rs -C panic=abort
 // #[cfg(not(panic = "unwind"))]
 // fn ah() {
 //     println!("This is not your party. Run!!!!");
@@ -93,4 +95,50 @@
 // fn main() {
 //     drink("water");
 //     drink("lemonade");
+// }
+// The adult has seen it all, and can handle any drink well.
+// All drinks are handled explicitly using `match`.
+// fn give_adult(drink: Option<&str>) {
+//     // Specify a course of action for each case.
+//     match drink {//here inner is just a name of the variable so dont get 
+//         //confused by the look of it
+//         Some("lemonade") => println!("Yuck! Too sugary."),
+//         Some(inner)   => println!("{}? How nice.", inner),
+//         None          => println!("No drink? Oh well."),
+//     }
+// }
+//
+// // Others will `panic` before drinking sugary drinks.
+// // All drinks are handled implicitly using `unwrap`.
+// fn drink(drink: Option<&str>) {
+//     // `unwrap` returns a `panic` when it receives a `None`.
+//     let inside = drink.unwrap();
+//     if inside == "lemonade" { panic!("AAAaaaaa!!!!"); }
+//
+//     println!("I love {}s!!!!!", inside);
+// }
+//
+// fn main() {
+//     let water  = Some("water");
+//     let lemonade = Some("lemonade");
+//     let void  = None;
+//
+//     give_adult(water);
+//     give_adult(lemonade);
+//     give_adult(void);
+//
+//     let coffee = Some("coffee");
+//     let nothing = None;
+//
+//     drink(coffee);
+//     drink(nothing);
+// }
+// fn next_birthday(current_age: Option<u8>) -> Option<String> {
+//     // If `current_age` is `None`, this returns `None`.
+//     // If `current_age` is `Some`, the inner `u8` value + 1
+//     // gets assigned to `next_age`
+//     let next_age: u8 = current_age? + 1;
+//     //here teh current_age? ? will just unwrap the value inside teh Option an the val here will 
+//     //be u8
+//     Some(format!("Next year I will be {}", next_age))
 // }
