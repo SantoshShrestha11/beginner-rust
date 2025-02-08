789ff703a756a387902fa27a797e6dc878d49931
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-02-08 11:25:38 +0545

    some head ach

---
+++ thread_usage/src/main.rs
@@ -0,0 +1,12 @@
+use std::thread;
+use std::time::Duration;
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowly ..");
+    thread::sleep(Duration::from_secs(4));
+    println!("Done calculating");
+    intensity
+}
+fn main() {
+ simulated_expensive_calculation(14);
+}
