9e5f9fd0d58531b19f118d4685af7e056ef3f74f
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-02-09 22:38:31 +0545

    learned the workspace of the rust and it was really fun here

--- add/adder/src/main.rs
+++ add/adder/src/main.rs
@@ -1,3 +1,12 @@
+// #[allow(unused_imports)]
+// use add_one;
+// use rand;
+// use add_two;
+
 fn main() {
-    println!("Hello, world!");
+    let num = 10;
+    println!("Hello, world! {num} plus one is {}!", add_one::add_one(num));
+
+    let another_num = 20;
+    println!("Hello, world! {another_num} plus two is {}!", add_two::add_two(another_num));
 }
