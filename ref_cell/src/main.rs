08b3ff0a699286246e99b101972a4d48ffae5c52
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-02-12 17:15:48 +0545

    some more

--- ref_cell/src/main.rs
+++ ref_cell/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let a: i32 = 5;
+    // here a is not mutable so we can take a reference 
+    let b: &mut i32 =  &mut a;
+
+    let mut c: i32 = 10;
+    // here c is mutable but hte  d is not so we can't take a reference and change it's value 
+    let d:&i32 = &c;
+    // here the reference self is non mutable  
+    *d = 20;
 }
