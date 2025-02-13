8ba57528fc51fc2af27934a8a4133421ab6c8d5e
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-02-13 07:00:53 +0545

    some more about the borrow

--- ref_cell/src/main.rs
+++ ref_cell/src/main.rs
@@ -1,11 +1,14 @@
 fn main() {
-    let a: i32 = 5;
-    // here a is not mutable so we can take a reference 
-    let b: &mut i32 =  &mut a;
+    // let a: i32 = 5;
+    // // here a is not mutable so we can take a reference 
+    // let b: &mut i32 =  &mut a;
+    //
+    // let mut c: i32 = 10;
+    // // here c is mutable but hte  d is not so we can't take a reference and change it's value 
+    // let d:&i32 = &c;
+    // // here the reference self is non mutable  
+    // *d = 20;
 
-    let mut c: i32 = 10;
-    // here c is mutable but hte  d is not so we can't take a reference and change it's value 
-    let d:&i32 = &c;
-    // here the reference self is non mutable  
-    *d = 20;
+    let c: i32 = 6;
+    let d: &i32 = &c;
 }
