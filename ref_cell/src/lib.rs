61502d289c57e82d835159f572eaa0fae8143854
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-02-14 17:47:36 +0545

    some book example which I cant understand

--- ref_cell/src/lib.rs
+++ ref_cell/src/lib.rs
@@ -2,35 +2,70 @@ pub trait Messenger {
     fn send(&self, msg: &str);
 }
 
-pub struct LimitTracker<'a, T:Messenger> {
-    messanger: &'a T,
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
     value: usize,
-    max: usize
+    max: usize,
 }
 
 impl<'a, T> LimitTracker<'a, T>
-    where
-T:Messenger,
+where
+    T: Messenger,
 {
-    pub fn new(messanger: &T, max: usize) -> LimitTracker<T> {
+    pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
         LimitTracker {
-            messanger,
+            messenger,
             value: 0,
-            max
+            max,
+        }
+    }
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+
+        let percentage_of_max = self.value as f64 / self.max as f64;
+
+        if percentage_of_max >= 1.0 {
+            self.messenger.send("Error: You are over your quota!");
+        } else if percentage_of_max >= 0.9 {
+            self.messenger
+                .send("Urgent warning: You've used up over 90% of your quota!");
+        } else if percentage_of_max >= 0.75 {
+            self.messenger
+                .send("Warning: You've used up over 75% of your quota!");
         }
     }
 }
 
-pub fn set_value(&mut self, value: usize) {
-    self.value = value;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let percentage_of_max = self.value as f64 / self.max as f64;
+    struct MockMessenger {
+        sent_messages: Vec<String>,
+    }
 
-    if percentage_of_max > 1.0 {
-        self.messanger.send("Error: You are over your quota!");
-    }else if percentage_of_max >= 0.9 {
-        self.menssanger.send("Urgent warning: You've used up ovr 90% of your quota!");
-    }else if percentage_of_max >= 0.75 {
-        self.messanger.send("Warning: You've used up over 75% of your quota!");
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: vec![],
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.sent_messages.len(), 1);
     }
 }
