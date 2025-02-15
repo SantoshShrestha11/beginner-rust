2ae6d4e5175a36431f9af0b343d0f16ec2973732
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-02-15 11:40:56 +0545

    done with the example of the mem leak for the rc and refcell

--- mem_leak/src/main.rs
+++ mem_leak/src/main.rs
@@ -18,4 +18,28 @@ impl List {
     }
 }
 fn main() {
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a initlal rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b)
+    }
+
+    println!("b rc count after changeing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+
+    // here this line will create the stack over flow error
+    // because here this cycle with a poinitng to b and b pointing to aand so forth until it
+    // overflows the stack
+    // println!("a next item = {:?}", a.tail());
+
 }
