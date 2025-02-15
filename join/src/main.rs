08f12ba2a1fcf47a85775b13fd7ef52d8d342455
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-02-15 23:17:49 +0545

    some correction of the type of the join here in the main.rs

--- join/src/main.rs
+++ join/src/main.rs
@@ -1,8 +1,8 @@
-use std::thread;
+use std::thread::{self, JoinHandle};
 use std::time::Duration;
 
 fn main() {
-    let handel = thread::spawn(||{
+    let handel: JoinHandle<()> = thread::spawn(||{
         for i in 1..=10 {
             println!("hi number {i} from the spawned thread!");
             thread::sleep(Duration::from_millis(1));
@@ -16,5 +16,6 @@ fn main() {
     //The two threads continue alternating,
     //but the main thread waits because of the call to handle.join() 
     //and does not end until the spawned thread is finished.
-    handel.join().unwrap()
+    handel.join().unwrap();
+    //here calling the unwrap because the type that it returns is a Result type
 }
