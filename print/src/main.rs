7f530ef67ffb11ee45f5d5dd10f671ea64d186ab
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-02-20 18:17:07 +0545

    some print

---
+++ print/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 34;
+    println!("Here is the number {}", x);
+    println!("Hello, world!");
+
+    let xa = vec![1, 3, 4, 1, 4, 12, 41];
+    println!("The vector list is: {:#?}", xa);
+    println!("The vector list is: {:?}", xa);
+    eprintln!("this is error here :(");
+}
